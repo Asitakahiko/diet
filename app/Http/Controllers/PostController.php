0b27e369e0b726978c83bc480993d22ca06a043b
Asitakahiko <dahaharu@icloud.com>
2021-08-18 10:01:27 +0900

    投稿消去

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -28,6 +28,7 @@ class PostController extends Controller
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
     }
+    
     public function edit(Post $post)
     {
         return view('edit')->with(['post' => $post]);
@@ -38,5 +39,11 @@ class PostController extends Controller
         $input_post = $request['post'];
         $post->fill($input_post)->save();
         return redirect('/posts/' . $post->id);
+    }
+    
+    public function delete(Post $post)
+    {
+    $post->delete();
+    return redirect('/posts');
     }
 }
